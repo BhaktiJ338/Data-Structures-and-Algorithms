c8e5ebfb0894ae558b253cd851a4385ac41ea4f5
Bhakti Jadhav <81022744+BhaktiJ338@users.noreply.github.com>
2022-04-13 20:25:48 +0530

    Create pascals_triangle.cpp

---
+++ pascals_triangle.cpp
@@ -0,0 +1,23 @@
+// leetcode - easy
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>vec{};
+        
+        for(int i = 0; i<numRows; i++){
+            vector<int>v{};
+            for(int j = 0; j<i+1; j++){
+                if(j==0 || j==i){
+                    v.push_back(1);
+                }
+                else{
+                    v.push_back(vec[i-1][j-1] + vec[i-1][j]);
+                }
+            }
+            vec.push_back(v);
+        }
+        return vec;
+    }
+   
+};

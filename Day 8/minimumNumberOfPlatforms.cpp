e5dd274dbfb4d988d5054455c4c6fd7dfc1ab8b5
Bhakti Jadhav <81022744+BhaktiJ338@users.noreply.github.com>
2022-07-24 14:41:30 +0530

    Create minimumNumberOfPlatforms.cpp

---
+++ Day 8/minimumNumberOfPlatforms.cpp
@@ -0,0 +1,25 @@
+	//Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+
+	int findPlatform(int arr[], int dep[], int n)
+    {
+    	sort(arr, arr+n);
+    	sort(dep, dep+n);
+    	
+    	int maxplat = 1, currplat = 1;
+    	int i=1, j=0;
+    	
+    	while(i<n && j<n){
+    	    //if next train is arriving before departure of prev train
+    	    if(arr[i] <= dep[j]){
+    	        currplat++;
+    	        i++;
+    	    }
+    	    else{
+    	        currplat--;
+    	        j++;
+    	    }
+    	    if(currplat > maxplat) maxplat = currplat;
+    	}
+    	return maxplat;
+    }
